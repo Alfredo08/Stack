93a6eb2a5e1a17238ecd64d770b0e47adcd920cd
Alfredo08 <alfredosv08@gmail.com>
2021-09-27 10:55:20 -0500

    Adding insert and remove methods

--- Stack.py
+++ Stack.py
@@ -3,3 +3,23 @@ from Node import Node
 class Stack:
     def __init__(self):
         self.top = None
+    
+    def insert( self, val ):
+        newNode = Node( val )
+        if self.top == None:
+            self.top = newNode
+        else:
+            newNode.next = self.top
+            self.top = newNode
+    
+    def remove( self ):
+        if self.top != None:
+            current = self.top
+            self.top = current.next
+            current.next = None
+    
+    def printStack( self ):
+        current = self.top
+        while current != None:
+            print( current.val )
+            current = current.next
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from Stack import Stack
 
 numsStack = Stack()
+
+numsStack.insert( 10 )
+numsStack.insert( 8 )
+numsStack.insert( 6 )
+numsStack.insert( 4 )
+numsStack.insert( 2 )
+
+numsStack.printStack()
+
+numsStack.remove()
+numsStack.remove()
+
+print( "-------" )
+numsStack.printStack()
